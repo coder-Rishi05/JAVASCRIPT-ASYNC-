04879dac8762d767a37387f4d4e280962bb6f58e
Rishabh Rawat <rawatrishi1121@gmail.com>
2025-05-21 14:05:27 +0530

    Update async.js

--- async.js
+++ async.js
@@ -1,10 +1,4 @@
-// // // 1. what is async
-// // // 2. what is  await.
-// // // 3. How it work behind the scene
-// // // 4. Example of using async / await.
-// // // 5. Error handling
-// // // 6. Interviews
-// // // 7. Asunc await vs promise .then and .catch
+
 
 // // // in next 1 week i ak thinking to revise js from akshay playlist of yt.
 
